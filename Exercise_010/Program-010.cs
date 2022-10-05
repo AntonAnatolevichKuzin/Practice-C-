caf981977eb999ad9fc049d4bebdfa014b94cbd3
Anton Kuzin <kupets1812@gmail.com>
2022-10-05 11:36:23 +0500

    Exercise-010 performed

--- Exercise_010/Program-010.cs
+++ Exercise_010/Program-010.cs
@@ -1,1 +1,14 @@
-﻿
+﻿ int varA;
+      while(true)
+      {
+      Console.Write("Введите трёхзначное число: ");
+        if(int.TryParse(Console.ReadLine(),out varA))
+         break;               
+      Console.WriteLine($"Ошибка ввода");
+      }
+
+      int f1 = varA/10;
+      int f2 = f1%10;
+
+      Console.WriteLine($"{varA} -> {f2}");
+
