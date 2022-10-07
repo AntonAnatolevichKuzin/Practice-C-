4e25788aa143d0b0f0b17a16dc99b132d1f4fda4
Anton Kuzin <kupets1812@gmail.com>
2022-10-07 13:08:36 +0500

    Exercise_013 performed

--- Exercise_013/Program-013.cs
+++ Exercise_013/Program-013.cs
@@ -4,3 +4,21 @@
 // 7 -> да
 // 1 -> нет
 
+int varA;
+while (true)
+{
+    Console.Write("Введите день недели: ");
+    if (int.TryParse(Console.ReadLine(), out varA))
+        break;
+    Console.WriteLine($"Ошибка ввода");
+}
+
+if (varA > 0 && varA < 6)
+{
+    Console.WriteLine($"{varA} -> нет");
+}
+    if (varA > 5 && varA < 8)
+    {
+        Console.WriteLine($"{varA} -> да");
+    }
+
