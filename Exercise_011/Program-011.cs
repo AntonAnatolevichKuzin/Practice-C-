1afc3201969d6c6f7545b34745157d36f3166741
Anton Kuzin <kupets1812@gmail.com>
2022-10-07 12:11:48 +0500

    Exercise-011 performed

--- Exercise_011/Program-011.cs
+++ Exercise_011/Program-011.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int varA;
+while (true)
+{
+    Console.Write("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out varA))
+        break;
+    Console.WriteLine($"Ошибка ввода");
+}
+
+
+if (varA < 100)
+{
+    Console.WriteLine($"{varA} -> третьей цифры нет");
+}
+else
+{
+    if (varA < 1000)
+    {
+        Console.WriteLine($"{varA} -> {(varA%100)%10}");
+    }
+    else
+    {
+        if (varA < 10000)
+        {
+            Console.WriteLine($"{varA} -> {(varA%100)/10}");
+        }
+        else
+        {
+            Console.WriteLine($"{varA} -> {(varA%1000)/100}");
+        }
+    }
+}
