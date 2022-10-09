f6bcd164e6060dbbddc72a91738b88c759bab9f4
Anton Kuzin <kupets1812@gmail.com>
2022-10-09 11:49:57 +0500

    Exercise_019 added Method

--- Exercise_019/Program-019.cs
+++ Exercise_019/Program-019.cs
@@ -4,23 +4,14 @@
 23432 -> да
 */
 
-int number;
-
-while (true)
+static void Execut(int number)
 {
-    Console.Write("Введите пятизначное число: ");
-    if (int.TryParse(Console.ReadLine(), out number))
-        if (number < 100000 && number > 9999)
-            break;
-    Console.WriteLine($"Ошибка ввода");
-}
-
-int n1 = number / 10000;
+    int n1 = number / 10000;
 int n2 = number / 1000 % 10;
 int n3 = number / 100 % 10;
 int n4 = number / 10 % 10;
 int n5 = number % 10;
-
+    
 
 if (n1 != n5 && n2 != n4)
 {
@@ -30,4 +21,16 @@ else
 {
     Console.Write($"{number} -> да");
 }
+}
 
+int number;
+while (true)
+{
+    Console.Write("Введите пятизначное число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        if (number < 100000 && number > 9999)
+            break;
+Console.WriteLine($"Ошибка ввода");
+}
+    
+Execut(number);
