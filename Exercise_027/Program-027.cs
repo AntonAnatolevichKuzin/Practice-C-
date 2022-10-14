e8ff56c527103975799ae6cc813eb9e9f37951dc
Anton Kuzin <kupets1812@gmail.com>
2022-10-14 13:42:29 +0500

    Exercise_027 performed

--- Exercise_027/Program-027.cs
+++ Exercise_027/Program-027.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ /* Напишите программу, которая принимает на вход число и выдаёт 
+ сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write($"{number} -> ");
+int result = 0;
+int count = 1;
+SummNumber(number);
+Console.Write($"{result}");
+
+
+
+int SummNumber(int number)
+{ 
+    while(number > 0)
+    {        
+        count = number%10;
+        result = result + count;
+        number = number/10;
+    }
+    return result;
+}
+
