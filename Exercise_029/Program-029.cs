ecaf851d24d30607f1b37ac431dff8645a6164ed
Anton Kuzin <kupets1812@gmail.com>
2022-10-14 15:19:36 +0500

    Exercise_029 performed

--- Exercise_029/Program-029.cs
+++ Exercise_029/Program-029.cs
@@ -3,3 +3,34 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+
+int[] array = GetInputNumbers(8);
+GetOutputNumbers(8);
+Console.Write($"{array[7]} -> [");
+GetOutputNumbers(8);
+Console.Write(array[7] + "]");
+
+// Методы
+
+int GetOutputNumbers(int Size)
+    {    
+      for (int i = 0; i < 7; i++)
+      {
+        Console.Write(array[i] + ", ");
+      }   
+      return array[7];
+    }
+ 
+
+
+  static int[] GetInputNumbers(int Size)
+  {
+    int[] num = new int[Size];
+    for (int i = 0; i < Size; i++)
+      {
+        Console.Write($"Введите {i +1} число массива: ");
+        num[i] = int.Parse(Console.ReadLine() ?? "");
+      }
+
+    return num;
+  }
