f903fdb4edc80bb275358c1b9f9698b9878193be
Anton Kuzin <kupets1812@gmail.com>
2022-10-16 00:37:45 +0300

    Exercise_038

--- Exercise_038/Program-038.cs
+++ Exercise_038/Program-038.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и 
+минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+double[] array = GetArray(6, 0, 10);
+PrintArray(array);
+
+
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+
+double[] GetArray(int size, int minValue, int maxValue)
+{
+    double[] res = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().NextDouble() * 100;
+    }
+
+    return res;
+}
