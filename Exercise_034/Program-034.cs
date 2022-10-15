1ba09b483c412155ff35213040a870b20c34ef4a
Anton Kuzin <kupets1812@gmail.com>
2022-10-16 01:20:11 +0500

    Exercise_034 performed

--- Exercise_034/Program-034.cs
+++ Exercise_034/Program-034.cs
@@ -3,3 +3,42 @@
 [345, 897, 568, 234] -> 2
 */
 
+int[] array = GetArray(8, 99, 1000);
+PrintArray(array);
+int evenNumbers = GetEvenNumbers(array);
+Console.Write($" -> {evenNumbers}");
+
+
+// Methods
+
+int GetEvenNumbers(int[] array)
+{
+    int evenNumbers = 0;
+    foreach(int el in array)
+    {
+        if(el%2 == 0) evenNumbers++;        
+    }
+    return evenNumbers;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return res;
+}
