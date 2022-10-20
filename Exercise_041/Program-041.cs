5bbd0cef98a92fe91d52b93f96c597907b18f9fe
Anton Kuzin <kupets1812@gmail.com>
2022-10-20 15:27:00 +0300

    Exercise_041 performed

--- Exercise_041/Program-041.cs
+++ Exercise_041/Program-041.cs
@@ -3,3 +3,47 @@
 1, -7, 567, 89, 223-> 3
 */
 
+Console.Write("Введите количество элементов массива: ");
+int size = int.Parse(Console.ReadLine() ?? "");
+int[] array = GetArray(size, -100000, 100000);
+PrintArray(array);
+int positiveNumber = PositiveNumber(array);
+Console.Write($" -> {positiveNumber}");
+
+
+// Methods
+
+int PositiveNumber(int[] arrey)
+{
+    int number = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if(arrey[i] > 0) number++;
+    }
+    return number;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите значение элемента массива: ");
+        res[i] = int.Parse(Console.ReadLine() ?? "");
+    }
+
+    return res;
+}
