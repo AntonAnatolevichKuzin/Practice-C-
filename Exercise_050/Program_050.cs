4876f04f356f61c0127ec878d241a46f9a04fd15
Anton Kuzin <kupets1812@gmail.com>
2022-10-24 22:33:56 +0300

    Exercise_050 performed

--- Exercise_050/Program_050.cs
+++ Exercise_050/Program_050.cs
@@ -6,3 +6,51 @@
 8 4 2 4
 */
 
+Console.Clear();
+Console.Write("Введите номер строки: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите номер столбца: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = GetArray(6, 6, 0, 10);
+SearchElement(array);
+PrintArray(array);
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void SearchElement(int[,] inArray)
+{
+    if (rows > inArray.GetLength(0) ^ columns > inArray.GetLength(1)) Console.Write($"Нет элемента с такими индексами");
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == rows - 1 && j == columns - 1) Console.Write($"Значение элемента с такими индексаими -> {inArray[i, j]} ");
+        }
+    }
+    Console.WriteLine();
+
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}    ");
+        }
+        Console.WriteLine();
+    }
+}
