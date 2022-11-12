8f717299b053add249ddbe8c408a85e9ca8eb207
Anton Kuzin <kupets1812@gmail.com>
2022-11-13 01:21:30 +0300

    Exercise_064 performed

--- Exercise_064/Program_064.cs
+++ Exercise_064/Program_064.cs
@@ -5,3 +5,17 @@ N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 */
 
+Console.WriteLine($"Введите целое число N:  ");
+int n = int.Parse(Console.ReadLine() ?? " ");
+PrintNaturals(1,n);
+Console.Write($"N = {n} -> ");
+Console.WriteLine(PrintNaturals(1,n));
+
+string PrintNaturals(int start, int end)
+{
+    if (start==end)
+    {
+        return end.ToString();
+    }
+    return (end + ", " + PrintNaturals(start, end -1));
+}
